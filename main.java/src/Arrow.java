277a9acb1cb042f0c4a8a376e2769ab01743ab01
Марина Князева <marishkai@yandex.ru>
2025-07-23 11:58:05 +0300

    Add files via upload

--- main.java/src/Arrow.java
+++ main.java/src/Arrow.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Arrow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char[] chars = scanner.nextLine().toCharArray();
+        System.out.print("Введите последовательность: ");
+        String input = scanner.nextLine();  // Ждёт ввода пользователя
+
+        String arrow1 = ">>-->";
+        String arrow2 = "<--<<";
         int count = 0;
 
-        for (int i = 0; i <= chars.length - 5; i++) {
-            if ((chars[i] == '>' && chars[i+1] == '>' && chars[i+2] == '-' &&
-                    chars[i+3] == '-' && chars[i+4] == '>') ||
-                    (chars[i] == '<' && chars[i+1] == '-' && chars[i+2] == '-' &&
-                            chars[i+3] == '<' && chars[i+4] == '<')) {
+        for (int i = 0; i <= input.length() - 5; i++) {
+            String sub = input.substring(i, i + 5);
+            if (sub.equals(arrow1) || sub.equals(arrow2)) {
                 count++;
             }
         }
 
-        System.out.println(count);
+        System.out.println("Найдено стрелок: " + count);
     }
 }
